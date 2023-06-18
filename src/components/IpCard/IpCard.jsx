55689678c85bf21b0b4adafea98d98e7ca35a12f
Riki <aguilcasariki@gmail.com>
2023-06-18 00:36:15 -0400

    Fix card width on mobile

--- src/components/IpCard/IpCard.jsx
+++ src/components/IpCard/IpCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 const IpCard = ({ cardData }) => {
   return (
-    <div className="ip_card_wrapper p-4 w-72 bg-white shadow z-50 -mt-32 rounded-lg">
+    <div className="ip_card_wrapper p-4 w-64 bg-white shadow z-50 -mt-32 rounded-lg">
       <div className="grid  grid-rows-4 md:grid-rows-none md:grid-cols-4 gap-y-3 ">
         <CardColumn columnData={cardData} />
       </div>
